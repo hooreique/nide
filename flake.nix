c40c65522fd41f897c326757f9d60f3d84eb7a6e
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-04-30 21:44:45 +0900

    initial commit

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: let
+    pkgs = inputs.nixpkgs.legacyPackages.${system};
+  in {
+    packages.default = pkgs.writeShellApplication {
+      name = "nide";
+      runtimeInputs = [ pkgs.jq ];
+      text = ''
+        DIR=$(nix flake metadata --no-write-lock-file --json | jq --raw-output '
+          if .original.path then
+            .original.path
+          elif .original.url | startswith("file://") then
+            .original.url | sub("^file://"; "")
+          else
+            error("illegal")
+          end
+        ')
+
+        NAME=$(basename "$DIR")
+
+        HASH=$(sha256sum <<< "$DIR" | cut -c1-8)
+
+        echo "$HOME/.nix-devshells/$NAME-$HASH" "$@"
+      '';
+    };
+  });
+}
