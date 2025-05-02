f10eb886a11092556e9a6600d4223d14259567f0
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-05-03 04:51:50 +0900

    add description

--- flake.nix
+++ flake.nix
@@ -1,6 +1,9 @@
 {
+  description = "nide for NIx DEvelop";
+
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system: let
     pkgs = inputs.nixpkgs.legacyPackages.${system};
   in {
