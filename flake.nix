b9247d1213de77c683b7ef833e865250aaa3683a
Song Jeyeong <46372718+hooreique@users.noreply.github.com>
2025-05-03 11:34:55 +0900

    misc

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "nide for NIx DEvelop";
+  description = "NIx DEvelop";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
